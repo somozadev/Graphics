27b2aea2cb2abb4e2413feeb3a67df298d2c560e
Marcos Somoza <somozadev@gmail.com>
2024-10-04 16:55:17 +0100

    updates unbinding buffer stuff

--- LearnOpenGL/Somine Engine/Somine/main.cpp
+++ LearnOpenGL/Somine Engine/Somine/main.cpp
@@ -174,10 +174,11 @@ int main()
     glVertexAttribPointer(2,2,GL_FLOAT, GL_FALSE, 8*sizeof(GLfloat), (void*)(6*sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
 
-    /* glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0); */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); 
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 
