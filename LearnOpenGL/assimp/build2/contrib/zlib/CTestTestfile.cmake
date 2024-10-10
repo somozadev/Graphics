0ba2ad2b250817261d6b8e49c547d2622ab5ce5f
Marcos Somoza <somozadev@gmail.com>
2024-10-10 12:10:12 +0100

    Assimp

---
+++ LearnOpenGL/assimp/build2/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mesom/Documents/GitHub/Graphics/LearnOpenGL/assimp/contrib/zlib
+# Build directory: C:/Users/mesom/Documents/GitHub/Graphics/LearnOpenGL/assimp/build2/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
